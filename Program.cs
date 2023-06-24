c099233ef9b8f98818652b26632605d5cf42e7a5
mostafaelnady <mostafaelnady1997@gmail.com>
2023-06-25 01:18:57 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Talabat.Repository.Data;
+
+namespace Talabat.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddDbContext<StoreContext>(options =>
+            {
+
+                options.UseSqlServer(builder.Configuration.GetConnectionString("default"));
+            });
+
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+
+
+            var app = builder.Build();
+
+            var logger = app.Services.GetRequiredService<ILoggerFactory>().CreateLogger<Program>();
+
+            try
+            {
+
+
+
+                var context = app.Services.GetRequiredService<StoreContext>();
+                context.Database.Migrate();
+            }
+            catch(Exception ex)
+            {
+                logger.LogError(ex,ex.Message);
+            }
+
+            // Configure the HTTP request pipeline.
+            if(app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
